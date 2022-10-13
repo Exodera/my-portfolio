b57551309c8284a44100ff1aa940624de2fe6c52
Oratile Ditlhage <96663869+Exodera@users.noreply.github.com>
2022-10-13 19:23:22 +0200

    Added starter files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 
 import './App.css';
+import Home from './pages/Home';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Experience from './pages/Experience';
+import Projects from './pages/Projects';
+import Header from './components/Header';
+
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path='/' element = {<Home/>}/>
+          <Route path='/Projects' element = {<Projects/>}/>
+          <Route path='/Experience' element = {<Experience/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
