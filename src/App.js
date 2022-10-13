ef08582e9712eba02f454f60cd4a6dda3c1b9a87
Oratile Ditlhage <96663869+Exodera@users.noreply.github.com>
2022-10-13 22:40:41 +0200

    Added footer and mobile responsiveness

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-import './App.css';
+import Footer from './components/Footer/Footer';
 import Home from './pages/Home';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Experience from './pages/Experience';
 import Projects from './pages/Projects';
-import Header from './components/Header';
+import Header from './components/Header/Header';
+import "./App.css"
 
 
 
@@ -18,6 +18,7 @@ function App() {
           <Route path='/Projects' element = {<Projects/>}/>
           <Route path='/Experience' element = {<Experience/>}/>
         </Routes>
+        <Footer/>
       </Router>
     </div>
   );
