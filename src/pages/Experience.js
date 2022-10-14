a2fc07ae60d91bf173eebecee5cd2a81a57fc4ca
Oratile Ditlhage <96663869+Exodera@users.noreply.github.com>
2022-10-14 05:58:38 +0200

    Updated Home and experience page

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,8 +1,43 @@
 import React from 'react'
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component'
+import "react-vertical-timeline-component/style.min.css"
+import {MdSchool} from 'react-icons/md'
+import "./styles/Experience.css"
+import {FaUniversity} from 'react-icons/fa'
+
+
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <div className='experience'>
+      <VerticalTimeline lineColor='rgb(30, 15, 98)'>
+        <VerticalTimelineElement 
+        className='timeline-education'
+        date="2014 - 2018"
+        icon={<MdSchool/>}
+        iconStyle = {{background: "rgb(30, 15, 98)", color: 'white'}} 
+        >
+          <h3 className='timeline-title'>
+          Attended High School Westonaria, Gauteng
+          </h3>
+          <p> Earned Matric Certificate</p> 
+        </VerticalTimelineElement>
+         
+         <VerticalTimelineElement
+         className='timeline-uni'
+         date ="2019 - Present"
+         icon ={<FaUniversity/>}
+         iconStyle={{background: "rgb(30, 15, 98)", color: "white"}}
+         >
+          <h3 className='timeline-title'> 
+            Attended Central University of Technology
+          </h3>
+          <p>Currently working on Diploma in Information Technology</p>
+
+         </VerticalTimelineElement>
+      </VerticalTimeline>
+
+    </div>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,40 @@
 import React from "react";
+import {AiFillGithub} from "react-icons/ai"
+import {AiFillLinkedin} from "react-icons/ai"
+import {AiFillMail} from "react-icons/ai"
+import "./styles/Home.css"
+
 
 const Home = ()=>{
     return (   
-    <div>
-        Home Page
+    <div className="home">
+        <div className="about">
+            <h2>Hi there, My name is Oratile</h2>
+            <p>
+                I am an aspiring website developer that 
+                loves learning and creating suiii
+            </p>
+            <div className="social-icons">
+                <AiFillGithub/>
+                <AiFillLinkedin/>
+                <AiFillMail/>
+            </div>
+         
+        </div>
+        <div className="skills">
+            <h1>Skills</h1>
+            <ol className="list">
+                <li className="item">
+                    <h2>Front-End</h2>
+                    <span>ReactJS, HTML, CSS, Figma</span>
+                </li>
+                <li className="item">
+                    <h2>Languages</h2>
+                    <span>JavaScript, C#, Dart</span>          
+                </li>
+            </ol>
+
+        </div>
     </div>
     )
  
